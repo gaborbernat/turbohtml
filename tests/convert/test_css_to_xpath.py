27072b1488a16bb0bc146aba1b61cf3f0ea8df97
Bernát Gábor <gaborjbernat@gmail.com>
2026-09-11 04:41:39 -0700

    test: consolidate conversion oracles

--- tests/convert/test_css_to_xpath.py
+++ tests/convert/test_css_to_xpath.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final
 
 import pytest
 
@@ -521,3 +521,159 @@ def test_specificity_rejects_an_invalid_selector() -> None:
 def test_specificity_rejects_a_non_string() -> None:
     with pytest.raises(TypeError):
         css_specificity(123)  # ty: ignore[invalid-argument-type]  # intentional non-str exercises the C argument guard
+
+
+_ORACLE_DOC: Final = """<!doctype html>
+<html id="html"><head>
+  <link id="link-href" href="foo">
+  <link id="link-nohref">
+</head><body>
+<div id="outer-div">
+ <a id="name-anchor" name="foo"></a>
+ <a id="tag-anchor" rel="tag" href="http://localhost/foo">link</a>
+ <a id="nofollow-anchor" rel="nofollow" href="https://example.org">link</a>
+ <ol id="first-ol" class="a b c">
+   <li id="first-li">content</li>
+   <li id="second-li" lang="En-us"><div id="li-div"></div></li>
+   <li id="third-li" class="ab c"></li>
+   <li id="fourth-li" class="ab
+c"></li>
+   <li id="fifth-li"></li>
+   <li id="sixth-li"></li>
+   <li id="seventh-li">  </li>
+ </ol>
+ <p id="paragraph">
+   <b id="p-b">hi</b> <em id="p-em">there</em>
+   <b id="p-b2">guy</b>
+   <input type="checkbox" id="checkbox-unchecked">
+   <input type="checkbox" id="checkbox-disabled" disabled>
+   <input type="text" id="text-checked" checked="checked">
+   <input type="checkbox" id="checkbox-checked" checked>
+   <input type="checkbox" id="checkbox-disabled-checked" disabled checked>
+ </p>
+ <ol id="second-ol"></ol>
+</div>
+<div id="foobar-div" foobar="ab bc
+cde"><span id="foobar-span"></span></div>
+</body></html>
+"""
+
+_ORACLE_SELECTORS: Final = (
+    "*",
+    "div",
+    "div div",
+    "div, div div",
+    "a[name]",
+    "a[rel]",
+    'a[rel="tag"]',
+    'a[href*="localhost"]',
+    'a[href*=""]',
+    'a[href^="http"]',
+    'a[href^="http:"]',
+    'a[href$="org"]',
+    'div[foobar~="bc"]',
+    'div[foobar~="cde"]',
+    '[foobar~="ab bc"]',
+    '[foobar~=""]',
+    "li:nth-child(3)",
+    "li:nth-child(10)",
+    "li:nth-child(2n)",
+    "li:nth-child(even)",
+    "li:nth-child(2n+1)",
+    "li:nth-child(odd)",
+    "li:nth-child(2n+4)",
+    "li:nth-child(3n+1)",
+    "li:nth-child(-n+3)",
+    "li:nth-child(-2n+4)",
+    "li:nth-child(n)",
+    "li:nth-child(-n)",
+    "li:nth-last-child(1)",
+    "li:nth-last-child(2n)",
+    "li:nth-last-child(2n+2)",
+    "ol:first-of-type",
+    "ol:nth-child(1)",
+    "ol:nth-of-type(2)",
+    "ol:nth-last-of-type(1)",
+    "span:only-child",
+    "li div:only-child",
+    "div *:only-child",
+    "p:only-of-type",
+    "a:empty",
+    ":root",
+    "html:root",
+    "li:root",
+    ".a",
+    ".c",
+    "ol *.c",
+    "ol li.c",
+    "li ~ li.c",
+    "ol > li.c",
+    "#first-li",
+    "li#first-li",
+    "*#first-li",
+    "li div",
+    "div > div",
+    "div>.c",
+    "div + div",
+    "a ~ a",
+    'a[rel="tag"] ~ a',
+    "ol#first-ol li:last-child",
+    "ol#first-ol *:last-child",
+    "#outer-div:first-child",
+    "#outer-div :first-child",
+    "a[href]",
+    ":not(*)",
+    "a:not([href])",
+    "ol :not(li[class])",
+    "link:has(*)",
+    "ol:has(div)",
+    ":is(#first-li, #second-li)",
+    "a:is(#name-anchor, #tag-anchor)",
+    ":is(.c)",
+    "ol.a.b.c > li.c:nth-child(3)",
+    ":checked",
+    ":lang(en)",
+    ":visited",
+    r"di\a0 v",
+    r"div\[",
+    r"[h\a0 ref]",
+    r"[h\]ref]",
+)
+
+
+@pytest.mark.parametrize("selector", [pytest.param(selector, id=selector) for selector in _ORACLE_SELECTORS])
+@pytest.mark.oracle
+def test_matches_the_cssselect_oracle(selector: str) -> None:
+    lxml_html: Final = pytest.importorskip("lxml.html")
+    cssselect: Final = pytest.importorskip("cssselect")
+    ours = turbohtml.parse(_ORACLE_DOC)
+    theirs = lxml_html.document_fromstring(_ORACLE_DOC)
+    our_expression = css_to_xpath(selector)
+    oracle_ids = [node.get("id", "nil") for node in theirs.xpath(cssselect.HTMLTranslator().css_to_xpath(selector))]
+    assert _ids(ours.xpath(our_expression)) == oracle_ids
+    assert [node.get("id", "nil") for node in theirs.xpath(our_expression)] == oracle_ids
+
+
+@pytest.mark.parametrize(
+    ("selector", "ours_only", "theirs_only"),
+    [
+        pytest.param("li:empty", ["seventh-li"], [], id="empty-allows-whitespace-per-selectors-4"),
+        pytest.param('*[lang|="en"]', ["second-li"], [], id="lang-attr-is-case-insensitive-in-html"),
+        pytest.param("input[type=CHECKBOX]", ["checkbox-unchecked"], [], id="type-attr-is-case-insensitive-in-html"),
+    ],
+)
+@pytest.mark.oracle
+def test_documented_divergences_from_cssselect(
+    selector: str,
+    ours_only: list[str],
+    theirs_only: list[str],
+) -> None:
+    lxml_html: Final = pytest.importorskip("lxml.html")
+    cssselect: Final = pytest.importorskip("cssselect")
+    ours = turbohtml.parse(_ORACLE_DOC)
+    our_ids = set(_ids(ours.xpath(css_to_xpath(selector))))
+    oracle_expression = cssselect.HTMLTranslator().css_to_xpath(selector)
+    oracle_ids = {node.get("id", "nil") for node in lxml_html.document_fromstring(_ORACLE_DOC).xpath(oracle_expression)}
+    assert our_ids - oracle_ids >= set(ours_only)
+    assert oracle_ids - our_ids == set(theirs_only)
+    assert our_ids == set(_ids(ours.select(selector)))
--- tests/convert/test_transform.py
+++ tests/convert/test_transform.py
@@ -4771,3 +4771,184 @@ def test_rule_benchmark_output(library: str, name: str, passes: int) -> None:
     _, _, load = next(benchmark for benchmark in benchmarks() if benchmark[0] == name)
     expected: Final = "".join(f"{ordinal}|" for ordinal in range(1, 1_025)) * passes
     assert str(operation(cast("tuple[str, str]", load()))) == expected
+
+
+def _oracle_sheet(body: str, *, method: str = "xml") -> str:
+    return f'<xsl:stylesheet version="1.0" {_NS}><xsl:output method="{method}"/>{body}</xsl:stylesheet>'
+
+
+_ORACLE_CATALOG: Final = (
+    '<catalog><book id="b1" cat="fiction"><title>Dune</title><price>9.99</price></book>'
+    '<book id="b2" cat="science"><title>Cosmos</title><price>12.50</price></book>'
+    '<book id="b3" cat="fiction"><title>1984</title><price>8.00</price></book></catalog>'
+)
+
+_ORACLE_CASES: Final = [
+    pytest.param(
+        "<r><name>World</name></r>",
+        _oracle_sheet('<xsl:template match="/">Hi <xsl:value-of select="//name"/></xsl:template>', method="text"),
+        {},
+        "text",
+        id="value-of-text",
+    ),
+    pytest.param(
+        _ORACLE_CATALOG,
+        _oracle_sheet(
+            '<xsl:template match="/"><ul>'
+            '<xsl:apply-templates select="catalog/book">'
+            '<xsl:sort select="title"/></xsl:apply-templates></ul></xsl:template>'
+            '<xsl:template match="book"><li class="{@cat}"><xsl:value-of select="title"/></li></xsl:template>',
+            method="html",
+        ),
+        {},
+        "html",
+        id="apply-templates-sort-html",
+    ),
+    pytest.param(
+        _ORACLE_CATALOG,
+        _oracle_sheet(
+            '<xsl:template match="/"><out>'
+            '<xsl:for-each select="catalog/book">'
+            '<item n="{position()}" id="{@id}"/></xsl:for-each></out></xsl:template>'
+        ),
+        {},
+        "xml",
+        id="for-each-position",
+    ),
+    pytest.param(
+        _ORACLE_CATALOG,
+        _oracle_sheet(
+            '<xsl:template match="/"><report>'
+            '<xsl:for-each select="catalog/book">'
+            '<xsl:sort select="price" data-type="number" order="descending"/>'
+            '<row><xsl:value-of select="position()"/>:<xsl:value-of select="title"/></row>'
+            "</xsl:for-each></report></xsl:template>"
+        ),
+        {},
+        "xml",
+        id="sort-number-descending",
+    ),
+    pytest.param(
+        _ORACLE_CATALOG,
+        _oracle_sheet(
+            '<xsl:key name="bycat" match="book" use="@cat"/>'
+            '<xsl:template match="/">'
+            "<counts fiction=\"{count(key('bycat','fiction'))}\" science=\"{count(key('bycat','science'))}\"/>"
+            "</xsl:template>"
+        ),
+        {},
+        "xml",
+        id="key",
+    ),
+    pytest.param(
+        "<doc><n>5</n></doc>",
+        _oracle_sheet(
+            '<xsl:template match="/"><xsl:choose>'
+            '<xsl:when test="doc/n &gt; 3">big</xsl:when><xsl:otherwise>small</xsl:otherwise>'
+            "</xsl:choose></xsl:template>",
+            method="text",
+        ),
+        {},
+        "text",
+        id="choose",
+    ),
+    pytest.param(
+        "<r/>",
+        _oracle_sheet(
+            '<xsl:template match="/"><xsl:call-template name="stars">'
+            '<xsl:with-param name="c" select="4"/></xsl:call-template></xsl:template>'
+            '<xsl:template name="stars"><xsl:param name="c"/>'
+            '<xsl:if test="$c &gt; 0">*<xsl:call-template name="stars">'
+            '<xsl:with-param name="c" select="$c - 1"/></xsl:call-template></xsl:if></xsl:template>',
+            method="text",
+        ),
+        {},
+        "text",
+        id="call-template-recursion",
+    ),
+    pytest.param(
+        '<a x="1"><b>t</b><c/></a>',
+        _oracle_sheet(
+            '<xsl:template match="@*|node()"><xsl:copy>'
+            '<xsl:apply-templates select="@*|node()"/></xsl:copy></xsl:template>'
+        ),
+        {},
+        "xml",
+        id="identity",
+    ),
+    pytest.param(
+        "<doc><s/><s/><s/></doc>",
+        _oracle_sheet(
+            '<xsl:template match="/"><list>'
+            '<xsl:for-each select="doc/s"><n><xsl:number format="i"/></n></xsl:for-each></list></xsl:template>'
+        ),
+        {},
+        "xml",
+        id="number-roman",
+    ),
+    pytest.param(
+        "<doc><v>1234.5</v></doc>",
+        _oracle_sheet(
+            '<xsl:template match="/"><out>'
+            "<xsl:value-of select=\"format-number(doc/v, '#,##0.00')\"/></out></xsl:template>"
+        ),
+        {},
+        "xml",
+        id="format-number",
+    ),
+    pytest.param(
+        "<doc/>",
+        _oracle_sheet(
+            '<xsl:param name="who" select="\'anon\'"/>'
+            '<xsl:template match="/"><greeting><xsl:value-of select="$who"/></greeting></xsl:template>'
+        ),
+        {"who": "'Alice'"},
+        "xml",
+        id="top-level-param",
+    ),
+    pytest.param(
+        _ORACLE_CATALOG,
+        _oracle_sheet(
+            '<xsl:template match="/"><xsl:apply-templates select="catalog/book"/></xsl:template>'
+            '<xsl:template match="book[@cat=\'fiction\']">F:<xsl:value-of select="title"/> </xsl:template>'
+            '<xsl:template match="book">O:<xsl:value-of select="title"/> </xsl:template>',
+            method="text",
+        ),
+        {},
+        "text",
+        id="conflict-resolution-predicate-priority",
+    ),
+]
+
+
+@pytest.mark.parametrize(("source", "sheet", "params", "method"), _ORACLE_CASES)
+@pytest.mark.oracle
+def test_transform_matches_lxml(source: str, sheet: str, params: dict[str, str], method: str) -> None:
+    mine = Transform(turbohtml.parse_xml(sheet))(turbohtml.parse_xml(source), **params)
+    etree: Final = pytest.importorskip("lxml.etree")
+    theirs = str(etree.XSLT(etree.fromstring(sheet.encode()))(etree.fromstring(source.encode()), **params))
+    if method == "text":
+        assert mine == theirs
+    else:
+        assert _canon(mine) == _canon(theirs)
+
+
+@pytest.mark.parametrize("attribute", ["count", "from"])
+@pytest.mark.oracle
+def test_number_benchmark_lxml_current_pattern_unsupported(attribute: str) -> None:
+    lxml: Final = pytest.importorskip("bench.competitors.lxml", exc_type=ImportError)
+    sheet, source = cast("tuple[str, str]", INPUTS["transform-number"]()[21][1])
+    with pytest.raises(NotImplementedError, match=r"XSLT 1\.0 forbids current\(\) in patterns"):
+        lxml.transform((sheet.replace('count="', f'{attribute}="'), source))
+
+
+@pytest.mark.oracle
+def test_number_benchmark_lxml_current_expression() -> None:
+    lxml: Final = pytest.importorskip("bench.competitors.lxml", exc_type=ImportError)
+    sheet: Final = (
+        '<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">'
+        '<xsl:output method="text"/><xsl:template match="/"><xsl:for-each select="root/p">'
+        '<xsl:number count="p"/><xsl:text>:</xsl:text><xsl:value-of select="current()/@id"/>'
+        "</xsl:for-each></xsl:template></xsl:stylesheet>"
+    )
+    assert str(lxml.transform((sheet, '<root><p id="7"/></root>'))) == "1:7"
